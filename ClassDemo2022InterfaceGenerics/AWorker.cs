c261e51290ee14654dc10b545daa18e9be49f729
Peter Levinsky <pele@easj.dk>
2022-02-15 12:45:18 +0100

    Understøtter generisk

--- ClassDemo2022InterfaceGenerics/AWorker.cs
+++ ClassDemo2022InterfaceGenerics/AWorker.cs
@@ -11,6 +11,7 @@ namespace ClassDemo2022InterfaceGenerics
 
         public void Start()
         {
+            /*
             IRegister register = new PersonRegister();
             register.Add(new Person(3000, "peter", "22334455"));
             register.Add(new Person(5000, "jakob", "33445566"));
@@ -23,7 +24,21 @@ namespace ClassDemo2022InterfaceGenerics
 
 
             Console.WriteLine(register);
+            */
 
+            PersonRegisterGeneric<Person> reg = new PersonRegisterGeneric<Person>();
+            reg.Add(new Person(3000, "peter", "22334455"));
+            reg.Add(new Person(5000, "jakob", "33445566"));
+            reg.Add(new Person(6000, "charlotte", "22334477"));
+
+            Console.WriteLine(reg);
+
+            PersonRegisterGeneric<String> reg2 = new PersonRegisterGeneric<String>();
+            reg2.Add("peter");
+            reg2.Add("jakob");
+            reg2.Add("charlotte");
+
+            Console.WriteLine(reg2);
         }
     }
 }
---
+++ ClassDemo2022InterfaceGenerics/model/PersonRegisterGeneric.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo2022InterfaceGenerics.model
+{
+    public class PersonRegisterGeneric<T> // sætter at den kan tage forskellige typer
+    {
+        private List<T> _list;
+
+        public PersonRegisterGeneric()
+        {
+            _list = new List<T>();
+        }
+
+        public List<T> GetAll()
+        {
+            return new List<T>(_list);
+        }
+
+        public T Add(T p)
+        {
+            _list.Add(p);
+            return p;
+        }
+
+        public T Delete(T p)
+        {
+            _list.Remove(p);
+            return p;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Liste : {string.Join(", ", _list)}";
+        }
+    }
+}
